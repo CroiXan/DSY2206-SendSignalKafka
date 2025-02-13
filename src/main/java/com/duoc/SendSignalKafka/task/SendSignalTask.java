9aff30b91a24cc4519d68ea291a2feb85fc0d48d
CroiXan <xan.mark.vi@gmail.com>
2025-02-13 00:11:24 -0300

    fecha en formato iso

--- src/main/java/com/duoc/SendSignalKafka/task/SendSignalTask.java
+++ src/main/java/com/duoc/SendSignalKafka/task/SendSignalTask.java
@@ -14,6 +14,7 @@ import com.duoc.SendSignalKafka.service.PatientService;
 import com.duoc.SendSignalKafka.service.VitalSignService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Component
@@ -56,6 +57,7 @@ public class SendSignalTask {
 
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
             try {
                 kafkaSenderService.sendMessage(objectMapper.writeValueAsString(newVitalSign));
